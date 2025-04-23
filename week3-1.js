b0d6bbe296740910641ebb27870f0c6e6bc85004
Xiantu <j6458221@yahoo.com.tw>
2025-04-23 21:30:19 +0800

    撰寫流程

---
+++ week3-1.js
@@ -0,0 +1,11 @@
+let purchaseRecords = [];
+
+function addPurchaseRecord(name, courses){
+  console.log(name,courses);
+  // 防呆驗證
+  // 購買數量套用優惠價格
+  // 計算總價
+  // 將資料寫入陣列 purchaseRecords
+  // 印出 console.log，印出「新增購買記錄成功！會員 [會員名稱] 購買 [數量] 堂課，總金額為 [金額] 元。」
+}
+addPurchaseRecord("Alice", 4);
