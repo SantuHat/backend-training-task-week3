ac4bfdedfe511a7bfc80e4b3442db807f23b1df0
Xiantu <j6458221@yahoo.com.tw>
2025-04-23 21:45:29 +0800

    練習-1

--- week3-1.js
+++ week3-1.js
@@ -1,11 +1,38 @@
 let purchaseRecords = [];
 
 function addPurchaseRecord(name, courses){
-  console.log(name,courses);
+  // console.log(name,courses);
   // 防呆驗證
   // 購買數量套用優惠價格
+  let price = 0;
+  if(courses <=10){
+    price = 1500;
+    // console.log(price);
+  }else if(courses <= 20){
+    price = 1300;
+    // console.log(price);
+  }else{
+    price = 1100;
+    // console.log(price);
+  }
+
   // 計算總價
+  let totalPrice = price * courses;
+  // console.log(totalPrice);
+  
   // 將資料寫入陣列 purchaseRecords
+  purchaseRecords.push({
+    name,
+    courses,
+    price,
+    totalPrice
+  })
+  // console.log(purchaseRecords);
+  
   // 印出 console.log，印出「新增購買記錄成功！會員 [會員名稱] 購買 [數量] 堂課，總金額為 [金額] 元。」
+  console.log(`新增購買記錄成功！會員 ${name} 購買 ${courses} 堂課，總金額為 ${totalPrice} 元。`);
+  
 }
+
 addPurchaseRecord("Alice", 4);
+addPurchaseRecord("Bob", 12);
